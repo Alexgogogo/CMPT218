88127c5db7b19a6e3c89894741c3a9f81039b342
Spear_Ace <959321146@qq.com>
2018-04-07 17:47:42 -0700

    fixed socket with temp solution

--- web/scripts/main.js
+++ web/scripts/main.js
@@ -1,17 +1,29 @@
 
 $( document ).ready(function(){
 
-  var socket = io.connect('http://localhost:8000',{reconnection:false});
-;
+  var socket = io.connect();
+
   socket.on('connect',function(){
     console.log('connected');
-
+    socket.emit('firstConnect');
   });
-
+  socket.on('login',function(data){
+    updateUserList(data);
+    if(data.playerNum ===1 || data.playerNum===2){
+      console.log('you are a player');
+    }
+    else{
+      console.log('you are a spectator');
+    }
+  });
 
   socket.on('userUpdate',function(users){
 
     updateUserList(users);
+  });
+  socket.on('userLeave',function(data){
+    updateUserList(data);
+    console.log(`${data.leaved} has left`);
   });
 });
 
